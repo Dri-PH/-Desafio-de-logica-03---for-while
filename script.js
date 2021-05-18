e3c4438481095c54fa2a971d4c9b0458a7d7ea71
Adriel Dias <you@example.com>
2021-05-18 18:13:41 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//1
+/*for (var plus = 1; plus <= 10; plus +=1){
+    console.log(plus);
+}
+
+//2
+let i = 10
+while(i >= 1){
+    console.log(i)
+    i -= 1 
+}
+
+//3
+for (var plus = 1; plus <= 100; plus +=2){
+    console.log(plus);
+}
+
+//4
+let j = 0
+while(j <= 100){
+    console.log(j)
+    j += 2
+}*/
